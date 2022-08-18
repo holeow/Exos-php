a168f8ab552848463715518427fece065be984c7
holeow <48766903+holeow@users.noreply.github.com>
2022-08-18 08:55:41 +0200

    exercice 9

---
+++ exercices/exo9/index.php
@@ -0,0 +1,30 @@
+<?php ob_start(); //  ne pas modifier
+$titre = "Exo 8 - La boucle while (remplacée par do-while)"; // Mettre le titre de la page
+?>
+
+<!-- mettre votre code ici -->
+<?php
+do{
+    $rand = rand(1,20);
+    $essais = 1;
+    if($rand <15){
+        echo "Essai $essais : $rand est trop petit (<15)";
+        echo "\n<br>\n";
+    }
+    $essais++;
+}
+while($rand <15);
+echo "Le nombre choisi est : $rand";
+
+
+
+?>
+
+<?php
+    /**
+     * Ne pa modifier
+     * permet d'inclure le menu et le template
+     */
+    $content = ob_get_clean(); 
+    require "../../partials/layout.php";
+?>
