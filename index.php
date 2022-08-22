089c220e5dd0371a29eddf715a512a999ab32d89
holeow <48766903+holeow@users.noreply.github.com>
2022-08-22 09:19:58 +0200

    changes

--- index.php
+++ index.php
@@ -5,11 +5,13 @@ $titre = "Exercices PHP"; // Mettre le titre de la page
 <!-- mettre le code ici -->
 <div class="row">
 
+    <h2 class="center-align">Les bases</h2>
+    <p>Afin de réaliser les exercices, vous avez à votre disposition une architecture de fichiers. Cette base de travail est téléchargeable sur Amio-Fit. Nous réaliserons la mise en place avec Laragon lors d’une séance. Dans la mesure du possible, il vous est demandé de respecter l’exemple de sortie donné en exemple.</p>
     <div class="col s12 m6 l4">
         <div class="card medium teal darken-2">
             <div class="card-content white-text">
-                <span class="card-title center-align">Exo1 - Variables</span>
-                <p>Déclaration de variables et permutation de leurs valeurs.</p>
+                <span class="card-title center-align">Exo01 - Variables</span>
+                <p>Cet exercice consiste à interchanger les valeurs de différentes variables</p>
             </div>
             <div class="card-action">
                 <a href="exercices/exo1" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
@@ -20,7 +22,7 @@ $titre = "Exercices PHP"; // Mettre le titre de la page
     <div class="col s12 m6 l4">
         <div class="card medium teal darken-2">
             <div class="card-content white-text">
-                <span class="card-title center-align">Exo2 - Variables et concaténation</span>
+                <span class="card-title center-align">Exo02 - Variables et concaténation</span>
                 <p>Vous devrez réaliser des affichages en utilisant différents types de concaténation</p>
             </div>
             <div class="card-action">
@@ -28,11 +30,10 @@ $titre = "Exercices PHP"; // Mettre le titre de la page
             </div>
         </div>
     </div>
-
     <div class="col s12 m6 l4">
         <div class="card medium teal darken-2">
             <div class="card-content white-text">
-                <span class="card-title center-align">Exo3 -  Les tests (1)</span>
+                <span class="card-title center-align">Exo03 - Les tests (1)</span>
                 <p>Écrire une expression conditionnelle utilisant les variables $age et $sexe dans une instruction if pour sélectionner une personne de sexe féminin dont l’age est compris entre 21 et 40 ans et afficher un message de bienvenue approprié.</p>
             </div>
             <div class="card-action">
@@ -40,11 +41,10 @@ $titre = "Exercices PHP"; // Mettre le titre de la page
             </div>
         </div>
     </div>
-
     <div class="col s12 m6 l4">
         <div class="card medium teal darken-2">
             <div class="card-content white-text">
-                <span class="card-title center-align">Exo4 - Les tests (2)</span>
+                <span class="card-title center-align">Exo04 - Les tests (2)</span>
                 <p>Le programme choisi un nombre aléatoire entre 1 et 20. En fonction de ce nombre il faudra construire une structure conditionnelle.</p>
             </div>
             <div class="card-action">
@@ -52,11 +52,10 @@ $titre = "Exercices PHP"; // Mettre le titre de la page
             </div>
         </div>
     </div>
-
     <div class="col s12 m6 l4">
         <div class="card medium teal darken-2">
             <div class="card-content white-text">
-                <span class="card-title center-align">Exo5 - Les tests (3)</span>
+                <span class="card-title center-align">Exo05 - Les tests (3)</span>
                 <p>Le programme choisi deux nombres aléatoires entre 1 et 100. Calcule la valeur absolue du premier moins le second et détermine si le résultat est compris entre 25 et 75.</p>
             </div>
             <div class="card-action">
@@ -64,11 +63,10 @@ $titre = "Exercices PHP"; // Mettre le titre de la page
             </div>
         </div>
     </div>
-
     <div class="col s12 m6 l4">
         <div class="card medium teal darken-2">
             <div class="card-content white-text">
-                <span class="card-title center-align">Exo6 - Les ternaires (3)</span>
+                <span class="card-title center-align">Exo06 - Les ternaires</span>
                 <p>Reprendre l'exo2 mais en utilisant les ternaires</p>
             </div>
             <div class="card-action">
@@ -76,11 +74,10 @@ $titre = "Exercices PHP"; // Mettre le titre de la page
             </div>
         </div>
     </div>
-
     <div class="col s12 m6 l4">
         <div class="card medium teal darken-2">
             <div class="card-content white-text">
-                <span class="card-title center-align">Exo7 - La boucle for</span>
+                <span class="card-title center-align">Exo07 - La boucle for</span>
                 <p>Le programme choisi un nombre aléatoire entre 5 et 15 puis affiche la table de multiplication de ce nombre.</p>
             </div>
             <div class="card-action">
@@ -88,11 +85,10 @@ $titre = "Exercices PHP"; // Mettre le titre de la page
             </div>
         </div>
     </div>
-
     <div class="col s12 m6 l4">
         <div class="card medium teal darken-2">
             <div class="card-content white-text">
-                <span class="card-title center-align">Exo8 - La boucle for</span>
+                <span class="card-title center-align">Exo08 - La boucle for</span>
                 <p>Le programme choisi un nombre aléatoire entre 5 et 15 puis affiche le cumul des nombres en sens inverse.</p>
             </div>
             <div class="card-action">
@@ -100,11 +96,10 @@ $titre = "Exercices PHP"; // Mettre le titre de la page
             </div>
         </div>
     </div>
-
     <div class="col s12 m6 l4">
         <div class="card medium teal darken-2">
             <div class="card-content white-text">
-                <span class="card-title center-align">Exo9 - La boucle while</span>
+                <span class="card-title center-align">Exo09 - La boucle while</span>
                 <p>Le programme choisi un nombre aléatoire entre 1 et 20 tant que le nombre choisi n'est pas supérieur à 15. Afficher les essais.</p>
             </div>
             <div class="card-action">
@@ -112,7 +107,6 @@ $titre = "Exercices PHP"; // Mettre le titre de la page
             </div>
         </div>
     </div>
-
     <div class="col s12 m6 l4">
         <div class="card medium teal darken-2">
             <div class="card-content white-text">
@@ -124,7 +118,6 @@ $titre = "Exercices PHP"; // Mettre le titre de la page
             </div>
         </div>
     </div>
-
     <div class="col s12 m6 l4">
         <div class="card medium teal darken-2">
             <div class="card-content white-text">
@@ -136,7 +129,6 @@ $titre = "Exercices PHP"; // Mettre le titre de la page
             </div>
         </div>
     </div>
-
     <div class="col s12 m6 l4">
         <div class="card medium teal darken-2">
             <div class="card-content white-text">
@@ -152,9 +144,7 @@ $titre = "Exercices PHP"; // Mettre le titre de la page
         <div class="card medium teal darken-2">
             <div class="card-content white-text">
                 <span class="card-title center-align">Exo13 - Les fonctions</span>
-                <p>Vous écrirez une fonction qui retourne la factorielle d’un nombre. La factorielle de n est la multiplication de tous les entiers <= n (ex : 5 !=1x2x3x4x5).<br>
-Notez que 0! et 1! = 1.<br>
-La fonction ne doit pas réaliser l'affichage.</p>
+                <p>Vous écrirez une fonction qui retourne la factorielle d’un nombre. La factorielle de n est la multiplication de tous les entiers <= n (ex : 5 !=1x2x3x4x5). Notez que 0! et 1!=1. La fonction ne doit pas réaliser l'affichage.</p>
             </div>
             <div class="card-action">
                 <a href="exercices/exo13" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
@@ -172,15 +162,206 @@ La fonction ne doit pas réaliser l'affichage.</p>
             </div>
         </div>
     </div>
-
-
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo15 - Les tableaux</span>
+                <p>Ecrire une fonction supprime les voyelles d'un mot.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo15" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo16 - Les tableaux</span>
+                <p>Vous allez créer deux tableaux contenant respectivement des prénoms masculins et des prénoms féminins.
+                    Vous afficherez ensuite le contenu des deux tableaux.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo16" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo17 - Les tableaux</span>
+                <p>Vous allez créer deux tableaux contenant des notes puis vous afficherez la moyenne des notes de chaque tableau.
+                    Vous afficherez ensuite le contenu des deux tableaux.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo17" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo18 - Les tableaux</span>
+                <p>Vous allez créer un tableau contenant des tableaux de notes. Vous afficherez ensuite la moyenne des notes de chaque tableau.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo18" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo19 - Les tableaux</span>
+                <p>Vous allez créer un tableau associatif contenant deux personnes. Pour chaque personne vous aurez un nom, un âge et un sexe.
+                    Vous afficher ensuite grâce à une fonction le contenu des tableaux.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo19" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo20 - Les tableaux et les fonctions</span>
+                <p>Vous allez utiliser des tableaux de tableaux et des fonctions.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo20" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo21 - Formulaire et méthode GET</span>
+                <p>Réalisez un formulaire HTML avec deux champs (pseudo et âge) puis affichez sur la même page le résultat saisi.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo21" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo22 - Formulaire et méthode POST</span>
+                <p>Réalisez un formulaire HTML avec un champ nombre puis affichez sur la même page si le nombre saisi est pair ou pas.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo22" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo23 - Formulaire et tableau</span>
+                <p>Vous réaliserez un formulaire (méthode POST) permettant de saisir 3 notes. Le programme principal récupérera les trois notes et les placera dans un tableau. Il calculera et affichera ensuite la moyenne de ces notes .</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo23" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <h2 class="center-align">BONUS</h2>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo24 - Tableau à 2 dimensions</span>
+                <p>Afficher l’ensemble des tables de multiplications (de 1 à 10) en utilisant un tableau à deux dimensions.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo24" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo25 - Fonctions dans un fichier externe</span>
+                <p>Dans un fichier externe, vous écrirez des fonctions permettant de travailler sur des tableaux.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo25" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo26 - Tableaux associatifs</span>
+                <p>Conditions sur tableaux associatifs.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo26" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <h2 class="center-align">POO</h2>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo27 - Les objets</span>
+                <p>A partir de l’exercice précédent (exo26) modifier la structure pour utiliser des objets.
+                    Créer une classe avec des attributs en public.
+                    Réaliser les affichages demandés.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo27" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo28 - Private et Getters</span>
+                <p>Créer une classe permettant de gérer des voitures.
+                    Chaque voiture comporte un modèle, une marque, une couleur et une année.
+                    Les attributs doivent être private.
+                    Permettre à l’utilisateur de filtrer les voitures par marque et par année.
+                    Vous utiliserez la méthode POST.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo28" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo29 - Attribut statique</span>
+                <p>Créer une classe permettant de gérer des maisons. La classe sera dans un fichier séparé du fichier index.php.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo29" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m6 l4">
+        <div class="card medium teal darken-2">
+            <div class="card-content white-text">
+                <span class="card-title center-align">Exo30 - Manipuler 2 classes</span>
+                <p>Créer 2 classes (Arme et Joueur) dans des fichiers séparés.
+                    Nous allons manipuler les 2 classes.</p>
+            </div>
+            <div class="card-action">
+                <a href="exercices/exo30" class="valign-wrapper">exercice <i class="material-icons">arrow_forward</i></a>
+            </div>
+        </div>
+    </div>
 </div>
 
+
 <?php
-    /**
-     * Ne pa modifier
-     * permet d'inclure le menu et le template
-     */
-    $content = ob_get_clean(); 
-    require "./partials/layout.php";
+/**
+ * Ne pa modifier
+ * permet d'inclure le menu et le template
+ */
+$content = ob_get_clean();
+require "./partials/layout.php";
 ?>
